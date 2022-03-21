c34af6dbbf2608207111bc8abfc12d0cd1723ca3
MoteoDimassi <82730892+MoteoDimassi@users.noreply.github.com>
2022-03-21 20:58:07 +0300

    Add files via upload
    наивный алгоритм поиска подстроки в строке

---
+++ Наивный_Алгоритм/Наивный_Алгоритм/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Наивный_Алгоритм
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "ааааа";
+            Console.WriteLine("Введите подстроку: ");
+            string find = Console.ReadLine();
+            List<int> index = simpleSearch(str, find);
+            if (index.Count >0)
+            {
+                Console.WriteLine("Подстрока содержится в строке " + index.Count+" раз");
+                foreach(var item in index)
+                {
+                    Console.WriteLine("На " + (item + 1) + " месте");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такой подстроки нет! :(");
+            }
+
+        }
+        public static List<int> simpleSearch(string where, string what)
+        {
+            List<int> enter = new List<int>();
+            int n = where.Length;
+            int m = what.Length;
+            bool flag = false;
+            int count = 0;
+
+            for (int i = 0; i< n - m+1; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    count++;
+                    if (where[i + j] == what[j])
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                        
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    enter.Add(i);
+                }
+                
+            }
+            Console.WriteLine(count + " - количество вхождений в циклы");
+            return enter;
+        }
+    }
+ }
+
